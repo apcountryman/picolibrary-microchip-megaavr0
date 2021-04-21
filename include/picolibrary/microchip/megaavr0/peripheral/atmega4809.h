a52c5bea01559f2a5e26b3de25cb6413bd8b5819
Andrew Countryman <apcountryman@gmail.com>
2021-04-21 15:40:22 -0700

    Add PORT peripheral
    Supports #28.

--- include/picolibrary/microchip/megaavr0/peripheral/atmega4809.h
+++ include/picolibrary/microchip/megaavr0/peripheral/atmega4809.h
@@ -24,6 +24,7 @@
 #define PICOLIBRARY_MICROCHIP_MEGAAVR0_PERIPHERAL_ATMEGA4809_H
 
 #include "picolibrary/microchip/megaavr0/peripheral/clkctrl.h"
+#include "picolibrary/microchip/megaavr0/peripheral/port.h"
 
 /**
  * \brief Microchip megaAVR 0-series ATmega4809 peripherals.
@@ -42,6 +43,60 @@ namespace picolibrary::Microchip::megaAVR0::Peripheral::ATmega4809 {
  *            picolibrary::Microchip::megaAVR0::Peripheral::CLKCTRL0.
  */
 using CLKCTRL0 = CLKCTRL_Instance<0x0060>;
+
+/**
+ * \brief PORTA.
+ *
+ * \attention This type should never be used directly. Instead, set the `-mmcu` compiler
+ *            flag to `atmega4809` and use
+ *            picolibrary::Microchip::megaAVR0::Peripheral::PORTA.
+ */
+using PORTA = PORT_Instance<0x0400>;
+
+/**
+ * \brief PORTB.
+ *
+ * \attention This type should never be used directly. Instead, set the `-mmcu` compiler
+ *            flag to `atmega4809` and use
+ *            picolibrary::Microchip::megaAVR0::Peripheral::PORTB.
+ */
+using PORTB = PORT_Instance<0x0420>;
+
+/**
+ * \brief PORTC.
+ *
+ * \attention This type should never be used directly. Instead, set the `-mmcu` compiler
+ *            flag to `atmega4809` and use
+ *            picolibrary::Microchip::megaAVR0::Peripheral::PORTC.
+ */
+using PORTC = PORT_Instance<0x0440>;
+
+/**
+ * \brief PORTD.
+ *
+ * \attention This type should never be used directly. Instead, set the `-mmcu` compiler
+ *            flag to `atmega4809` and use
+ *            picolibrary::Microchip::megaAVR0::Peripheral::PORTD.
+ */
+using PORTD = PORT_Instance<0x0460>;
+
+/**
+ * \brief PORTE.
+ *
+ * \attention This type should never be used directly. Instead, set the `-mmcu` compiler
+ *            flag to `atmega4809` and use
+ *            picolibrary::Microchip::megaAVR0::Peripheral::PORTE.
+ */
+using PORTE = PORT_Instance<0x0480>;
+
+/**
+ * \brief PORTF.
+ *
+ * \attention This type should never be used directly. Instead, set the `-mmcu` compiler
+ *            flag to `atmega4809` and use
+ *            picolibrary::Microchip::megaAVR0::Peripheral::PORTF.
+ */
+using PORTF = PORT_Instance<0x04A0>;
 
 } // namespace picolibrary::Microchip::megaAVR0::Peripheral::ATmega4809
 

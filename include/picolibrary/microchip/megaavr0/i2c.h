67b3fe72ccbc4e4721674df0d2821844f1a45e75
Andrew Countryman <apcountryman@gmail.com>
2022-03-08 16:23:34 -0800

    Add Microchip megaAVR 0-series I2C facilities namespace (#549)
    Resolves #528 (Add Microchip megaAVR 0-series I2C facilities namespace).

---
+++ include/picolibrary/microchip/megaavr0/i2c.h
@@ -0,0 +1,32 @@
+/**
+ * picolibrary-microchip-megaavr0
+ *
+ * Copyright 2021-2022, Andrew Countryman <apcountryman@gmail.com> and the
+ * picolibrary-microchip-megaavr0 contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Microchip::megaAVR0::I2C interface.
+ */
+
+#ifndef PICOLIBRARY_MICROCHIP_MEGAAVR0_I2C_H
+#define PICOLIBRARY_MICROCHIP_MEGAAVR0_I2C_H
+
+/**
+ * \brief Microchip megaAVR 0-series Inter-Integrated Circuit (I2C) facilities.
+ */
+namespace picolibrary::Microchip::megaAVR0::I2C {
+} // namespace picolibrary::Microchip::megaAVR0::I2C
+
+#endif // PICOLIBRARY_MICROCHIP_MEGAAVR0_I2C_H

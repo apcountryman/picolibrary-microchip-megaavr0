8528f29e4abf057d179a99cc8593a7a3b71c5e60
Andrew Countryman <apcountryman@gmail.com>
2022-04-29 22:29:32 -0700

    Add Microchip megaAVR 0-series device info print interactive test (#697)
    Resolves #693 (Add Microchip megaAVR 0-series device info print
    interactive test).

---
+++ test/interactive/picolibrary/microchip/megaavr0/device_info/print/main.cc
@@ -0,0 +1,50 @@
+/**
+ * picolibrary-microchip-megaavr0
+ *
+ * Copyright 2021-2022, Andrew Countryman <apcountryman@gmail.com> and the
+ * picolibrary-microchip-megaavr0 contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Microchip::megaAVR0::Device_Info print interactive test program.
+ */
+
+#include "picolibrary/testing/interactive/microchip/megaavr0/clock.h"
+#include "picolibrary/testing/interactive/microchip/megaavr0/device_info.h"
+#include "picolibrary/testing/interactive/microchip/megaavr0/log.h"
+
+namespace {
+
+using ::picolibrary::Testing::Interactive::Microchip::megaAVR0::configure_clock;
+using ::picolibrary::Testing::Interactive::Microchip::megaAVR0::Log;
+using ::picolibrary::Testing::Interactive::Microchip::megaAVR0::Device_Info::print;
+
+} // namespace
+
+/**
+ * \brief Execute the picolibrary::Microchip::megaAVR0::Device_Info print interactive
+ *        test.
+ *
+ * \return N/A
+ */
+int main() noexcept
+{
+    configure_clock();
+
+    Log::initialize();
+
+    print( Log::instance() );
+
+    for ( ;; ) {} // for
+}
